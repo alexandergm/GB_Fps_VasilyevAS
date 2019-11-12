b4960d036202052103783e0853d80039a76dbf02
alexandergm <sasha_261@outlook.com>
2019-11-12 23:27:49 +0300

    les1

---
+++ Lesson/Assets/Scripts/View/FlashLightUi.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Geekbrains
+{
+    public sealed class FlashLightUi : MonoBehaviour
+    {
+        public Slider _slider;
+        // image
+
+        private void Awake()
+        {
+            _slider = GetComponent<Slider>();
+        }
+
+       
+        public void SetActive(bool value)
+        {
+            _slider.gameObject.SetActive(value);
+        }
+    }
+}
